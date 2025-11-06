a39c6c9345ad23bacee419065ab0b1bf6a430447
vladimirulyr-cmyk <vladimirulyr@gmail.com>
2025-11-06 14:54:29 +0500

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Добро пожаловать в моё FastAPI-приложение!"}
+    return {"message": "Добро пожаловать в моё FastAPI! Запуск через Vercel!"}
 
 @app.get("/user/{username}")
 def greet_user(username: str):
